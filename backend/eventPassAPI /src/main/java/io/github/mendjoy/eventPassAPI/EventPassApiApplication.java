2a4dd8943c8c90acb0f5224e0f6534b714ee11c4
mendjoy <joycemendes.jm79@gmail.com>
2024-09-20 10:44:03 -0300

    setup inicial backend

---
+++ backend/eventPassAPI /src/main/java/io/github/mendjoy/eventPassAPI/EventPassApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.mendjoy.eventPassAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventPassApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventPassApiApplication.class, args);
+	}
+
+}
---
+++ backend/eventPassAPI /src/test/java/io/github/mendjoy/eventPassAPI/EventPassApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.mendjoy.eventPassAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventPassApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
