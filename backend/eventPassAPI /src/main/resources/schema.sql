31774cb358a3ac39d844b234d941e880a4701809
mendjoy <joycemendes.jm79@gmail.com>
2024-09-23 10:40:04 -0300

    schema user

---
+++ backend/eventPassAPI /src/main/resources/schema.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE eventpass;
+
+CREATE TABLE user (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+     username VARCHAR(50) NOT NULL UNIQUE,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    birth_date DATE,
+    phone VARCHAR(20),
+    admin BOOLEAN DEFAULT FALSE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
