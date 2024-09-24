898ed32a18a39dfb30b37dd1159abd85194bb877
mendjoy <joycemendes.jm79@gmail.com>
2024-09-24 11:50:10 -0300

    ajustes componente de mensagem de erro

--- frontend/event-pass-ingressos/src/components/ErrorMessage/ErrorMessage.js
+++ frontend/event-pass-ingressos/src/components/ErrorMessage/ErrorMessage.js
@@ -4,9 +4,13 @@ import styles from './ErrorMessage.module.css'
 const ErrorMessage = ({ message }) => {
   if (!message) return null 
 
+  const errorLines = message.split('\n')
+
   return (
     <div className={styles.errorContainer}>
-        <p>{message}</p>
+        {errorLines.map((line, index) => (
+            <p key={index}>{line}</p> 
+        ))}
     </div>
   )
 }
