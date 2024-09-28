576f7d74c92f9fbc29e9fbb76b0a58a2110e399e
mendjoy <joycemendes.jm79@gmail.com>
2024-09-28 15:10:31 -0300

    alterando navbar quando usuario estiver autenticado

--- frontend/event-pass-ingressos/src/components/Navbar/Navbar.js
+++ frontend/event-pass-ingressos/src/components/Navbar/Navbar.js
@@ -1,3 +1,5 @@
+import React, { useState, useEffect } from 'react'
+
 //css
 import  styles from "./Navbar.module.css";
 import { Link } from 'react-router-dom'
@@ -8,8 +10,25 @@ import { BsCalendar4Event } from "react-icons/bs";
 import { IoTicketOutline } from "react-icons/io5";
 import { FaRegUser } from "react-icons/fa";
 import { IoSearch } from "react-icons/io5";
+import { MdOutlineExpandMore } from "react-icons/md";
+import { RiLogoutBoxRLine } from "react-icons/ri";
+
+//context
+import { useAuth } from "../../context/AuthContext"
 
 const Navbar = () => {
+
+    const { user, logout } = useAuth()
+    const [dropdownVisible, setDropdownVisible] = useState(false)
+
+    const toggleDropDown = () => {
+        setDropdownVisible(!dropdownVisible)
+    }
+
+    useEffect(() => {
+        setDropdownVisible(false)
+    }, [user])
+
     return (
         <div className={styles.navbarContainer}>
             <div>
@@ -32,10 +51,26 @@ const Navbar = () => {
             
             <div>
                 <nav className={styles.navbarLinks}>
-                    <Link to="/"><GrHomeRounded className="icon"/> Home</Link>
+                    <Link to="/"><GrHomeRounded   className="icon"/> Home</Link>
                     <a href="#"><BsCalendar4Event className="icon"/> Eventos</a>
-                    <a href="#"><IoTicketOutline className="icon"/> Ingressos</a>
-                    <Link to="/login"><FaRegUser className="icon"/> Login</Link>
+                    <a href="#"><IoTicketOutline  className="icon"/> Ingressos</a>
+
+                    { user ? 
+                        ( <div className={styles.dropdown}>
+                            <span onClick={ toggleDropDown }><FaRegUser className="icon"/> {user} <MdOutlineExpandMore /></span> 
+                            
+                            {dropdownVisible && (
+                                <div className={styles.dropdownMenu}>
+                                    <Link to="/profile" className={styles.dropdownItem}>Minha Conta</Link>
+                                    <Link to="#" className={styles.dropdownItem}>Meus Ingressos</Link>
+                                    <Link to="#" className={styles.dropdownItem}>Favoritos</Link>
+                                    <button onClick={ logout } className={styles.dropdownItem}> <RiLogoutBoxRLine /> Sair</button>
+                                </div>
+                            )}
+                         </div>) :
+                        ( <Link to="/login"><FaRegUser className="icon"/> Login</Link>)
+                    }
+        
                 </nav>
             </div> 
         </div>
