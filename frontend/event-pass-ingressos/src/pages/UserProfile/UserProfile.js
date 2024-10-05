b188db2e841bbf1e1d7356490b36a131219cc8f4
mendjoy <joycemendes.jm79@gmail.com>
2024-10-05 19:30:09 -0300

    pagina de detalhes dos dados do usuario

--- frontend/event-pass-ingressos/src/pages/UserProfile/UserProfile.js
+++ frontend/event-pass-ingressos/src/pages/UserProfile/UserProfile.js
@@ -1,16 +1,63 @@
+import { useEffect, useState } from "react"
 import styles from "./UserProfile.module.css"
+
+//components
+import ErrorMessage from '../../components/ErrorMessage/ErrorMessage'
 
 const UserProfile = () => {
 
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState("")
+    const [username, setUsername] = useState("")
+    const [birthDate, setBirthDate] = useState("")
+    const [phone, setPhone] = useState("")
+    const [errorMessage, setErrorMessage] = useState('')
+
+    const getUserDetails = async () => {
+
+        const token = localStorage.getItem('token');
+       
+        try {
+            const response = await fetch(`/user/profile`, {
+                method: "GET",
+                headers: {
+                    'Content-Type' : 'application/json',
+                    'Authorization': `Bearer ${token}`
+                },
+            })
+
+            const data = await response.json()
+
+            let newDateObj = new Date(data.birthDate)
+            const formattedDate = newDateObj.toISOString().split("T")[0]
+
+            setName(data.name)
+            setUsername(data.username)
+            setEmail(data.email)
+            setBirthDate(formattedDate)
+            setPhone(data.phone)
+
+        } catch (error) {
+            setErrorMessage(`Ocorreu um erro: ${error}`)
+        }
+    }
+
+    useEffect(() => {
+        getUserDetails()
+    }, [])
+
     return (
-        <div className={styles.profileContainer}>
+        <div className={styles.profileContainer}>            
+            {errorMessage && ( <ErrorMessage message={errorMessage} />) }
             <h2 className={styles.title}>Dados da Conta</h2>
             <div className={styles.profileDetail}>
                 <label htmlFor="name">Nome</label>
                 <input 
                     type="text" 
                     id="name" 
-                    className={styles.inputField} />
+                    className={styles.inputField} 
+                    value={name}
+                    onChange={(e) => { setName(e.target.value) }}/>
             </div>
             <div className={styles.profileDetail}>
                 <label htmlFor="name">Nome de Usuario</label>
@@ -18,7 +65,17 @@ const UserProfile = () => {
                     type="text" 
                     id="name" 
                     className={styles.inputField} 
-                    readOnly/>
+                    readOnly
+                    defaultValue={username}/>
+            </div>
+            <div className={styles.profileDetail}>
+                <label htmlFor="birthDate">Data de Nascimento</label>
+                <input 
+                    type="date" 
+                    id="birthDate"
+                    className={styles.inputField} 
+                    readOnly
+                    defaultValue={birthDate}/>
             </div>
             <div className={styles.profileDetail}>
                 <label htmlFor="email">Email</label>
@@ -26,22 +83,17 @@ const UserProfile = () => {
                     type="email" 
                     id="email" 
                     className={styles.inputField} 
-                    readOnly/>
+                    readOnly
+                    defaultValue={email}/>
             </div>
             <div className={styles.profileDetail}>
                 <label htmlFor="phone">Telefone</label>
                 <input 
                     type="tel" 
                     id="phone" 
-                    className={styles.inputField} />
-            </div>
-            <div className={styles.profileDetail}>
-                <label htmlFor="registrationDate">Data de Cadastro</label>
-                <input 
-                    type="date" 
-                    id="registrationDate"
                     className={styles.inputField} 
-                    readOnly/>
+                    value={phone}
+                    onChange={(e) => { setPhone(e.target.value) }}/>
             </div>
             <div className={styles.buttonContainer}>
                 <button className={styles.saveButton}>Salvar Dados</button>
