e29d498f963ce62d33ef5a5e6a9d4e70c185ec79
mendjoy <joycemendes.jm79@gmail.com>
2024-10-21 21:45:17 -0300

    refatorando authcontext

--- frontend/event-pass-ingressos/src/context/AuthContext.js
+++ frontend/event-pass-ingressos/src/context/AuthContext.js
@@ -1,32 +1,47 @@
-import React, { createContext, useContext, useState } from 'react'
+import React, { createContext, useContext, useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 
 const AuthContext = createContext()
 
 export const AuthProvider = ({ children }) => {
     
-    const [user, setUser] = useState(localStorage.getItem("username"))
-    const [token, setToken] = useState(localStorage.getItem("token"))
+    const [user, setUser] = useState(null)
+    const [token, setToken] = useState(null)
+    const [authority, setAuthority] = useState(null)
 
     const navigate = useNavigate()
 
-    const login = (username, authToken) => {
+    const login = (username, authToken, authority) => {
         setUser(username) 
         setToken(authToken)
+        setAuthority(authority)
         localStorage.setItem("username", username)
         localStorage.setItem("token", authToken)
+        localStorage.setItem("authority", authority)
     }
 
     const logout = () => {
         setUser(null)
         setToken(null)
-        localStorage.removeItem("username")
-        localStorage.removeItem("token")
+        setAuthority(null)
+        localStorage.clear()
         navigate("/")
     }
 
+    useEffect(() => {
+        const storedUser  = localStorage.getItem("username")
+        const storedToken = localStorage.getItem("token")
+        const storedAuthority = localStorage.getItem("authority")
+
+        if(storedUser && storedToken && storedAuthority){
+            setUser(storedUser)
+            setToken(storedToken)
+            setAuthority(storedAuthority)
+        }
+    }, [])
+
     return (
-        <AuthContext.Provider value={{ user, login, logout }}>
+        <AuthContext.Provider value={{ user, token, authority, login, logout }}>
             {children} 
         </AuthContext.Provider>
     )
