a093d556b701e2f9d356db0c4071747e1643dbef
mendjoy <joycemendes.jm79@gmail.com>
2024-10-27 19:37:46 -0300

    tratando excecoes de acesso negado

--- backend/eventPassAPI /src/main/java/io/github/mendjoy/exception/GlobalExceptionHandler.java
+++ backend/eventPassAPI /src/main/java/io/github/mendjoy/exception/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import io.github.mendjoy.dto.response.ResponseApi;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -28,6 +29,12 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(DuplicateKeyException.class)
     public ResponseEntity<ResponseApi> handleDuplicateKeyException(DuplicateKeyException exception){
         ResponseApi responseApi = new ResponseApi(HttpStatus.CONFLICT,exception.getMessage(), true);
+        return new ResponseEntity<>(responseApi, responseApi.getStatus());
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<ResponseApi> handleAccessDeniedException(AccessDeniedException exception){
+        ResponseApi responseApi = new ResponseApi(HttpStatus.FORBIDDEN, exception.getMessage(), true);
         return new ResponseEntity<>(responseApi, responseApi.getStatus());
     }
 }
