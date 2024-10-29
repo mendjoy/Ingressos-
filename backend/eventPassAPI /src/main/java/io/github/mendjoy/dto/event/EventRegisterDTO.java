ccd3a3155715be761caa22d169b160771b456f84
mendjoy <joycemendes.jm79@gmail.com>
2024-10-28 21:12:19 -0300

    event dto

---
+++ backend/eventPassAPI /src/main/java/io/github/mendjoy/dto/event/EventRegisterDTO.java
@@ -0,0 +1,108 @@
+package io.github.mendjoy.dto.event;
+
+import java.time.LocalTime;
+import java.util.Date;
+
+public class EventRegisterDTO {
+
+    private Integer id;
+
+    private String name;
+    private String description;
+
+    private Date eventDate;
+
+    private LocalTime startTime;
+
+    private LocalTime endTime;
+
+    private String location;
+
+    private Integer capacity;
+
+    private String urlImage;
+
+    public EventRegisterDTO() {
+
+    }
+
+    public EventRegisterDTO(String name, String description, Date eventDate, LocalTime startTime, LocalTime endTime, String location, Integer capacity, String urlImage) {
+        this.name = name;
+        this.description = description;
+        this.eventDate = eventDate;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.location = location;
+        this.capacity = capacity;
+        this.urlImage = urlImage;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getEventDate() {
+        return eventDate;
+    }
+
+    public void setEventDate(Date eventDatedate) {
+        this.eventDate = eventDatedate;
+    }
+
+    public LocalTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public LocalTime getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(LocalTime endTime) {
+        this.endTime = endTime;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Integer getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(Integer capacity) {
+        this.capacity = capacity;
+    }
+
+    public String getUrlImage() {
+        return urlImage;
+    }
+
+    public void setUrlImage(String urlImage) {
+        this.urlImage = urlImage;
+    }
+
+}
