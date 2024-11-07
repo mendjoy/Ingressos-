c74fe058a81a785679448068e34984d7ee48d427
mendjoy <joycemendes.jm79@gmail.com>
2024-11-07 20:56:25 -0300

    pagina inicial com cards de eventos

--- frontend/event-pass-ingressos/src/app/page.js
+++ frontend/event-pass-ingressos/src/app/page.js
@@ -1,12 +1,67 @@
 "use client"
 
-import Navbar from "../components/Navbar/Navbar";
-import { AuthProvider } from "../context/AuthContext";
+import { useEffect, useState } from "react"
+import { useRouter } from 'next/navigation' 
 
-export default function Home() {
-  return (
+import getData from "@/service/api/getData"
+import EventCard from "@/components/EventCard/EventCard"
+
+//components
+import ErrorMessage   from "@/components/ErrorMessage/ErrorMessage"
+import SuccessMessage from "@/components/SuccessMessage/SuccessMessage"
+
+const Home = () =>{
+
+    const [events, setEvents] = useState([])
+    const [successMessage, setSuccessMessage] = useState("")
+    const [errorMessage, setErrorMessage] = useState("")
+
+    const router = useRouter()
+
+    const getEvents = async () => {
+
+        try {
+
+            const response = await getData("/event")
+
+            if(response.error){
+                    
+                setErrorMessage(response.message)
+
+            }else{
+
+                setEvents(response.data.content)
+
+            }
+        } catch (error) {
+
+            setErrorMessage(error.message)
+        }
+    }
+
+    useEffect(() => {
+        
+        getEvents()
+
+    }, [])
+  
+    return (
     <div>
-      
+        {errorMessage   && ( <ErrorMessage message={errorMessage} />) }
+        {successMessage && ( <SuccessMessage message={successMessage}/> )}
+        <div className="cardContainer">
+            {events.map((event) => (
+            <EventCard
+                key={event.id}
+                name={event.name}
+                date={event.eventDate}
+                location={event.location}
+                urlImage={event.urlImage}
+                onClick={() => { router.push(`/event/${event.id}`)}}/>
+          ))}
+        </div>
     </div>
   )
 }
+
+export default Home
