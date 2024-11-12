27a7667cee9872abd266721b2e3acb6c25886338
mendjoy <joycemendes.jm79@gmail.com>
2024-11-11 21:14:53 -0300

    ajuste query method do evento

--- backend/eventPassAPI /src/main/java/io/github/mendjoy/repository/EventRepository.java
+++ backend/eventPassAPI /src/main/java/io/github/mendjoy/repository/EventRepository.java
@@ -1,10 +1,9 @@
 package io.github.mendjoy.repository;
 
-import io.github.mendjoy.dto.event.EventDTO;
 import io.github.mendjoy.entity.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EventRepository extends JpaRepository<Event, Integer> {
 
-    public EventDTO findById(int id);
+    public Event findById(int id);
 }
