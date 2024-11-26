5a429d8883a30d5b11961ac118b8494fc8631b40
mendjoy <joycemendes.jm79@gmail.com>
2024-11-25 21:13:56 -0300

    utilizando session storage

--- frontend/event-pass-ingressos/src/context/AuthContext.js
+++ frontend/event-pass-ingressos/src/context/AuthContext.js
@@ -1,5 +1,4 @@
-import React, { createContext, useContext, useEffect, useState } from 'react'
-//import { useRouter } from "next/router"
+import React, { createContext, useContext, useEffect, useState } from "react"
 
 const AuthContext = createContext()
 
@@ -8,29 +7,27 @@ export const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null)
     const [token, setToken] = useState(null)
     const [authority, setAuthority] = useState(null)
-    //const router = useRouter()
 
     const login = (username, authToken, authority) => {
         setUser(username) 
         setToken(authToken)
         setAuthority(authority)
-        localStorage.setItem("username", username)
-        localStorage.setItem("token", authToken)
-        localStorage.setItem("authority", authority)
+        sessionStorage.setItem("username", username)
+        sessionStorage.setItem("token", authToken)
+        sessionStorage.setItem("authority", authority)
     }
 
     const logout = () => {
         setUser(null)
         setToken(null)
         setAuthority(null)
-        localStorage.clear()
-       // router.push("/")
+        sessionStorage.clear()
     }
 
     useEffect(() => {
-        const storedUser  = localStorage.getItem("username")
-        const storedToken = localStorage.getItem("token")
-        const storedAuthority = localStorage.getItem("authority")
+        const storedUser  = sessionStorage.getItem("username")
+        const storedToken = sessionStorage.getItem("token")
+        const storedAuthority = sessionStorage.getItem("authority")
 
         if(storedUser && storedToken && storedAuthority){
             setUser(storedUser)
