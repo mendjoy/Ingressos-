d9d873ebf93061acbbe9757ca0db402df34f1456
mendjoy <joycemendes.jm79@gmail.com>
2024-12-11 21:34:16 -0300

    ajusteS

--- backend/eventPassAPI /src/main/java/io/github/mendjoy/repository/EventTicketTypeRepository.java
+++ backend/eventPassAPI /src/main/java/io/github/mendjoy/repository/EventTicketTypeRepository.java
@@ -1,6 +1,7 @@
 package io.github.mendjoy.repository;
 
+import io.github.mendjoy.entity.EventTicketType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EventTicketTypeRepository extends JpaRepository<Integer, EventTicketTypeRepository> {
+public interface EventTicketTypeRepository extends JpaRepository<EventTicketType, Integer> {
 }
